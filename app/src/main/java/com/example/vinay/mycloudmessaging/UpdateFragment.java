4bcc1aa18a00842b954752058a3da7e9400f4d21
vinay2897 <35741093+vinay2897@users.noreply.github.com>
2018-08-25 16:17:22 +0530

    Testing first commit

--- app/src/main/java/com/example/vinay/mycloudmessaging/UpdateFragment.java
+++ app/src/main/java/com/example/vinay/mycloudmessaging/UpdateFragment.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -93,7 +92,6 @@ public class UpdateFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         LocalBroadcastManager.getInstance(this.getActivity()).unregisterReceiver(mMessageReceiver);
-
     }
 
 }
